d142c2ddbc58d23df8fa36181a258619f0eadd3a
shawn sandy <shawnsandy04@gmail.com>
2016-08-02 15:32:35 -0400

    Landing page layout

--- resources/views/vendor/page/index.blade.php
+++ resources/views/vendor/page/index.blade.php
@@ -2,8 +2,7 @@
 @section('page')
     @include('shared.nav')
     <header class="index">
-        <section class="cover">
-
+        <div class="cover">
             <div class="layout-table">
                 <div class="layout-row">
                     <div class="logo">
@@ -11,27 +10,82 @@
                         <p class="lead">
                             Our really cool elevator pitch goes here!
                         </p>
-
                     </div>
                 </div>
             </div>
-
-
-        </section>
+        </div>
     </header>
     <main>
         <div class="container">
             <div class="row text-center">
                 <h1>Welcome Message</h1>
                 <p class="lead text-center">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias aliquid asperiores aspernatur et
-                    hic in incidunt, inventore iste iure modi nam natus nihil nisi omnis optio rem repellendus saepe
-                    sapiente voluptatum.
+                  Synergistically procrastinate sustainable intellectual capital rather than interdependent leadership skills. Objectively myocardinate high-quality content through team driven technology. Interactively syndicate 24/365 networks through virtual leadership. Monotonectally utilize competitive data before resource sucking infomediaries. Collaboratively pontificate stand-alone relationships after real-time benefits.
                 </p>
             </div>
-            <hr>
+
         </div>
 
+
+        <section>
+
+
+        <div class="container">
+                <div class="row">
+                    <div class="col-md-12 products text-center">
+                        <img src="/img/atlas-2.jpeg" alt="" class="img-responsive">
+                        <h2>Atlas For Workforce</h2>
+                        <p class="lead">
+                          Assertively initiate accurate architectures and synergistic materials. Rapidiously recaptiualize cost effective e-commerce via 2.0 "outside the box" thinking. Globally streamline frictionless human capital rather than one-to-one bandwidth. Objectively customize corporate.
+                        </p>
+                    </div>
+                    <div class="col-md-6 products">
+                        <img src="https://as2.ftcdn.net/jpg/00/99/59/79/500_F_99597971_TTaJxbCLXbSB7FEJLhJSSn4RZfHdzo6J.jpg" alt="" class="img-responsive">
+                        <h3 class="lead text-center">Atlas Intern and Recruitment</h3>
+                        <p>
+                          Globally recaptiualize highly efficient services for e-business technology. Conveniently enable state of the art alignments vis-a-vis professional e-business. Appropriately exploit out-of-the-box mindshare before premium vortals. Assertively formulate enabled models via.
+                        </p>
+                    </div>
+                    <div class="col-md-6 products">
+                        <img src="https://as2.ftcdn.net/jpg/00/99/59/79/500_F_99597971_TTaJxbCLXbSB7FEJLhJSSn4RZfHdzo6J.jpg" alt="" class="img-responsive">
+                        <h3 class="lead text-center">Web and Application Development</h3>
+                        <p>
+                          Enthusiastically leverage existing collaborative users after enterprise-wide web services. Distinctively reinvent customized functionalities after virtual growth strategies. Monotonectally maintain prospective e-markets through web-enabled portals.
+                        </p>
+                    </div>
+
+
+                    <div class="col-md-12 products text-center">
+                        <img src="/img/atlas-network-2.jpeg">
+                        <h1 class="">Network Management</h1>
+                        <p class="lead">
+                          Holisticly morph emerging innovation whereas mission-critical process improvements. Efficiently provide access to cross-media systems before tactical scenarios. Competently iterate interdependent data rather than quality "outside the box" thinking. Credibly actualize.
+                        </p>
+                    </div>
+                    <div class="col-md-6 products">
+                        <img src="https://as2.ftcdn.net/jpg/00/99/59/79/500_F_99597971_TTaJxbCLXbSB7FEJLhJSSn4RZfHdzo6J.jpg" alt="" class="img-responsive">
+                        <h3 class="lead text-center">IT Sales and Support</h3>
+                        <p>
+                          Competently architect customer directed platforms without superior deliverables. Compellingly visualize functionalized methodologies without business convergence. Quickly innovate clicks-and-mortar value for team building channels. Seamlessly productize plug-and-play communities before resource-leveling scenarios.
+                        </p>
+                    </div>
+                    <div class="col-md-6 products">
+
+                        <img src="https://as2.ftcdn.net/jpg/00/99/59/79/500_F_99597971_TTaJxbCLXbSB7FEJLhJSSn4RZfHdzo6J.jpg" alt="" class="img-responsive">
+                        <h3 class="lead text-center">Embedded Systems</h3>
+                        <p>
+                          Enthusiastically deliver process-centric catalysts for change after bleeding-edge methods of empowerment. Competently extend parallel sources with parallel paradigms. Interactively leverage other's web-enabled alignments without team driven interfaces. Credibly matrix innovative.
+                        </p>
+                    </div>
+
+
+
+                </div>
+
+
+        </div>
+
+        </section>
     </main>
     <footer class="big-nav">
         <div class="layout-table">
