cfd4f43593ecd35a43f6b27142e8b5b15e16bb77
shawn sandy <shawnsandy04@gmail.com>
2016-08-24 10:43:32 -0400

    Updates the copy on the about page - update pagekit package
    e

--- resources/views/page/about.blade.php
+++ resources/views/page/about.blade.php
@@ -3,25 +3,40 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12 text-center">
-                <h1>About Us</h1>
                 <p class="lead">
-                    Globally innovate goal-oriented strategic theme areas rather than frictionless deliverables. Intrinsicly deliver resource maximizing initiatives rather than long-term high-impact markets. Uniquely.
+                    Complete Technology Solutions (CTS) was founded in 1997 by Maurice Ryman. Over the past 15 years
+                    Maurice has grown his company from a single employee with a one room office to a large dynamic
+                    company with many wonderful long-time employees.
                 </p>
             </div>
-            <div class="col-md-12">
-                <h2>We are the Cool Company</h2>
+
+            <div class="col-md-10">
+                <h1>About Us</h1>
+
                 <p>
-                    Efficiently unleash premium core competencies vis-a-vis backward-compatible architectures. Holisticly parallel task market-driven relationships through impactful collaboration and idea-sharing. Appropriately utilize wireless e-business without strategic channels. Phosfluorescently scale long-term high-impact meta-services without magnetic paradigms. Uniquely synergize compelling platforms through technically sound portals.
+                    Complete Technology Solutions (CTS) was founded in 1997 by Maurice Ryman. Over the past 15 years
+                    Maurice has grown his company from a single employee with a one room office to a large dynamic
+                    company with many wonderful long-time employees. After a few years of providing IT services
+                    specifically to the Workforce Development Industry, the CTS team noticed a need for integrated
+                    document management and service tracking for the Workforce Development Industry. Since the standard
+                    document management programs that were available were not catered toward Workforce development and
+                    lacked many of the components that One-Stops were needing and looking for.
                 </p>
 
-                <p>Progressively brand enabled portals vis-a-vis focused experiences. Authoritatively administrate value-added metrics through out-of-the-box ROI. Quickly negotiate collaborative synergy for integrated technologies. Rapidiously mesh team driven metrics before synergistic web services. Progressively matrix covalent imperatives through inexpensive manufactured products.
+                <p>
+                    Thanks to many talented minds, ATLAS is being utilized by numerous Workforce Boards and over
+                    1,000,000 One Stop customers and counting.
                 </p>
+
+                <p>
+                    As we fast forward to 2015/2016 we once again recognized the need for a dynamic easy to use collaboration platform that can connect Individuals and Employers to Opportunities. Our team got together to develop and launch the Atlas Intern & Recruitment (AIR) platform.
+                </p>
+
             </div>
             <div class="col-md-12">
                 <h2>Our Team</h2>
                 <hr>
                 <div class="row">
-
                     <div class="team-images text-center">
                         <img src="http://placehold.it/140x140" class="img-circle">
                         <img src="http://placehold.it/140x140" class="img-circle">
@@ -33,7 +48,8 @@
                 </div>
                 <hr>
                 <p>Aut culpa cum, debitis molestias neque voluptates!
-                    Iusto, magni, odio? Assumenda ducimus explicabo id inventore natus nihil nostrum officiis quos. Assumenda
+                    Iusto, magni, odio? Assumenda ducimus explicabo id inventore natus nihil nostrum officiis quos.
+                    Assumenda
                     excepturi impedit minus mollitia nam, qui, quo rem reprehenderit repudiandae similique soluta sunt,
                     voluptatibus?
                 </p>
