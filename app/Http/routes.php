9997b136a72f0f1a00fac007ded647de83b3d8f0
shawn sandy <shawnsandy04@gmail.com>
2017-01-05 14:58:22 -0500

    Delete old route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,39 +19,4 @@ Route::get('/pages/{name}', function ($name) {
     return view('page.' . $name);
 });
 
-use Crew\Unsplash as Unsplash;
-
-Route::get('unsplash', function () {
-    Unsplash\HttpClient::init([
-        'applicationId' => 'ff1c6c90eec52a82c30dbadbcb0acdc04e403d8a749e16a03909f6978b31d8a9',
-        'secret' => 'e89a4910547363b6693a49f17611242b51c34a9ac5bebaa9e95c1db43f62140a',
-    ]);
-    $scopes = ['public', 'write_user'];
-    Unsplash\HttpClient::$connection->getConnectionUrl($scopes);
-//    $cats = Unsplash\Photo::all();
-//    dump($cats->getArrayCopy());
-    $response = (new Unsplash\HttpClient())->send("get", ['/search/photos/?query=people']);
-    $body = collect(json_decode($response->getBody(), true));
-
-    $results = collect($body['results']);
-
-    dump($results);
-
-    $img = $results->map(function ($item, $key) {
-        return [
-            "username" => $item['user']['first_name'].' '.$item['user']['last_name'],
-            "id" => $item['user']['id'],
-            "avatar" => $item['user']['profile_image']['small'],
-            "photo_raw" => $item['urls']['raw'],
-            "photo_full" => $item['urls']['full'],
-            "photo_regular" => $item['urls']['regular'],
-            "photo_small" => $item['urls']['small'],
-            "photo_thumb" => $item['urls']['thumb']
-        ];
-    });
-
-
-    return dump($img);
-
-});
 
